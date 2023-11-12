7953f0ceb9db309362e18af452f485c7bcfd67f3
Kaustubh Vats <55024026+kaustubh-vats@users.noreply.github.com>
2023-11-12 22:14:11 +0530

    added meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const devMode = (process.env.NODE_ENV === "development");
-  const csp = `default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' ${devMode ? "'unsafe-eval'": ''}; font-src 'self' fonts.gstatic.com; img-src 'self'; connect-src 'self'`;
+  const csp = `default-src 'self'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; script-src 'self' ${devMode ? "'unsafe-eval'": ''}; font-src 'self' fonts.gstatic.com; img-src 'self'; connect-src 'self'`;
   return (
     <Html lang="en">
       <Head>
